4ee3da928da4f926a712425587d795fd0ac6ae20
Casper Andresen <casperb.andresen@gmail.com>
2020-11-18 10:44:41 +0100

    added actuator classes

--- src/workshopCLI/Actuator.java
+++ src/workshopCLI/Actuator.java
@@ -1,11 +1,51 @@
 package workshopCLI;
 
 public abstract class Actuator {
+    private String name;
+    private double value;
 
+    Actuator(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public void setValue(double newValue) {
+        this.value = newValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
 
-public class Ventilation extends Actuator {
+class Ventilator extends Actuator {
+    Ventilator(String name, double value){
+        super(name, value);
+    }
 
+
+    @Override
+    public void setValue(double newValue) {
+        super.setValue(newValue);
+    }
+
+    @Override
+    public String toString() {
+        return "Actuator{" +
+                "name='" + getName() + '\'' +
+                ", value=" + getValue() +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Ventilator v1 = new Ventilator("bedroomVentilator", 69);
+
+        System.out.println(v1.toString());
+    }
 }
 
 
